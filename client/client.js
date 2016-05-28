351b6e2e16044c09a05ba3c8668061480c5c07f5
Jenny Lian <jennylian100@gmail.com>
2016-05-27 21:25:10 -0700

    try to play with the first lil bit

---
+++ client/client.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { reder } from 'react-dom'
+import App from '../components/App'
+
+render(<App/>, document.getElementById('app'))
---
+++ components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component{
+  render (){
+    return <div> This is a react app now! </div>
+  }
+}
+
+export default App
