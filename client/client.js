71c7be2e6ef52db2d0e0e103c9545abb1d928c03
Jenny Lian <jennylian100@gmail.com>
2016-06-04 20:44:04 -0700

    adding second part of connecting the store to the app

--- client/client.js
+++ client/client.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
-
+import configureStore from '../redux/store'
+import { Provider } from 'react-redux'
 //configure and create the store here..reducers. initial state
-render(React.createElement(App) , document.getElementById('app'))
+let initialState = {
+  dim_sum_ordered: [{
+    id: 0,
+    delivered: false,
+    name: "shiu mai"
+  }]
+}
+
+let store = configureStore(initialState)
+render(<Provider store={store} ><App/></Provider> , document.getElementById('app'))
