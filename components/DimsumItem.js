4b6346aa737da4e122c6d4e56e7d46bf07d2ba60
Jenny Lian <jennylian100@gmail.com>
2016-06-18 07:19:19 -0700

    adding buttons actions for mark as arrived and cancel order

--- components/DimsumItem.js
+++ components/DimsumItem.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
 
-class DimSumItem extends Component{
+class DimSumItem extends Component {
 
-    render (){
+    handleArrived(e){
+        console.log("order arrived", e)
+    }
+
+    handleCancel(){
+        console.log("cancel order")
+    }
+    render() {
         return (
-
-           <li key={this.props.dim_sum.id}>{this.props.dim_sum.name}</li>
-
+            <div>
+                <li key={this.props.dim_sum.id}>{this.props.dim_sum.name}</li>
+                <button onClick={this.handleArrived.bind(this)}>Mark as arrived</button>
+                <button onClick={this.handleCancel.bind(this)}>Cancel Order</button>
+            </div>
         )
     }
 }
